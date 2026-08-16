0efcdc7007e0d7a3477ee6babadce1bb0e5cf6e5
Rowan <rowanhadegu@gmail.com>
2026-08-16 16:33:30 +0300

    add search and filter category with event handler and product grid

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import {useState, useEffect } from "react";
 
 
 export function Products(){
@@ -29,13 +29,66 @@ export function Products(){
 
     fetchProducts();
     },[]);
-
     
-
-    return(
-        <div>
-            <h1>Products</h1>
-        </div>
+    if (loading) return (
+        <p>Loading Products</p>
+    );
+    if (error) return (
+        <p>Error: {error}</p>
     )
-}
+
+    const categories = ['all', ...new Set(products.map((p) => p.category))];
+    const filteredProducts = products.filter((product) => {
+    const matchesSearch = product.title .toLowerCase() .includes(searchQuery.toLowerCase());      
+    const matchesCategory =selectedCategory === 'all' || product.category === selectedCategory;   
+    return matchesSearch && matchesCategory;
+  });
+ 
+
+
+return (
+    <div>
+      <h2>Products</h2>
+
+      {/* search  */}
+      <div>
+        <input
+          type="text"
+          placeholder="Search products..."
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+
+        <select
+          value={selectedCategory}
+          onChange={(e) => setSelectedCategory(e.target.value)}
+        >
+          {categories.map((cat) => (
+            <option key={cat} value={cat}>
+              {cat.toUpperCase()}
+            </option>
+          ))}
+        </select>
+      </div>
+{/* filter */}
+      <div>
+        {filteredProducts.length === 0 ? (
+          <p>No products found.</p>
+        ) : (
+          filteredProducts.map((product) => (
+            <div
+              key={product.id}
+              style={{ border: '1px solid #ccc', margin: '10px', padding: '10px' }}
+            >
+              <img src={product.image} alt={product.title} width="100" />
+              <h3>{product.title}</h3>
+              <p>${product.price}</p>
+              <p>Category: {product.category}</p>
+              <Link to={`/products/${product.id}`}>View Details</Link>
+            </div>
+          ))
+        )}
+      </div>
+    </div>
+  )};
 export default Products;
