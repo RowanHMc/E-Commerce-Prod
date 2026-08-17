720c73a95c2b1a1c84454cef7964e92f95f80c71
Rowan <rowanhadegu@gmail.com>
2026-08-17 13:47:56 +0300

    add Authcontext

---
+++ src/contex/AuthContext.jsx
@@ -0,0 +1,23 @@
+import { createContext, useContext, useState, useEffect } from "react";
+
+const AuthContext = createContext();
+
+export const AuthContext = ({children}) => {
+    const [user, setUser] = useState(null);
+    const login = (userData) => {
+        setUser(userData);
+    };
+
+    const logout = () => {
+        setUser(null);
+    };
+
+    const isLoggedIn = Boolean(user);
+    return(
+        <AuthContext.Provider value ={{user, isLoggedIn, login, logout}}>
+            {children}
+        </AuthContext.Provider>
+    )
+};
+
+export const useAuth =() => useContext(AuthContext);
