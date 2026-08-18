dd689c18613cff03bbd5be29447494ee6d87317e
Rowan <rowanhadegu@gmail.com>
2026-08-18 21:28:16 +0300

    add login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,48 @@
-function Login(){
-    return(
+import { useState } from "react";
+import { useAuth } from "../contex/AuthContext";
+
+
+
+export function Login(){
+    const[email, setEmail] = useState('');
+    const[password, setPassword] = useState('');
+    const[error, setError] = useState('');
+
+    const {login} = useAuth();
+  
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if(!email.trim() || !password.trim()){
+            setError('Input field empty');
+            return;
+        }
+        setError('');
+        login({email});
+
+    };
+    return( 
         <div>
             <h1>Login</h1>
+            {error && <p>{error}</p>}
+            <form onSubmit = {handleSubmit}>
+                <div>
+                    <label>Email:</label>
+                    <input type="email"
+                    value ={email}
+                    onChange = {(e) => setEmail(e.target.value)}/>
+                </div>
+                <div>
+                    <label>Password:</label>
+                    <input type="password"
+                    value ={password}
+                    onChange = {(e) => setPassword(e.target.value)}/>
+                </div>
+                <button type="submit">Log In</button>
+                
+            </form>
         </div>
     )
-}
+};
 
 export default Login;
