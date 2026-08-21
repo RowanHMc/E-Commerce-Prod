78993e24418f55de273574f017ef455f6d6490fa
Rowan <rowanhadegu@gmail.com>
2026-08-21 09:39:59 +0300

    import elements, add authprovider and cartprovider

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,36 @@
-import Checkout from "./pages/Checkout";
+import { Routes,Route } from "react-router-dom";
+import { AuthProvider } from "./contex/AuthContext";
+import { CartProvider } from "./contex/CartContext";
+import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+import Checkout from "./pages/Checkout";
 import Login from "./pages/Login";
 import ProductDetails from "./pages/ProductDetails";
 import Products from "./pages/Products";
 import Cart from "./pages/Cart";
+import ProtectedRoute from "./components/ProtectedRoute";
 
-function app(){
+function App(){
     return(
         <>
-        <Home/>
-        <Products/>
-        <ProductDetails/>
+        <AuthProvider>
+            <CartProvider>
+                <Navbar />
+                <Routes>
+                    <Route path="/" element= {<Home/>} />
+                    <Route path="/products" element= {<Products/>} />
+                    <Route path="/products/:id" element= {<ProductDetails/>} />
+                    <Route path="/cart" element= {<Cart/>} />
+                    
+                    <Route path="/checkout" element={<ProtectedRoute><Checkout/></ProtectedRoute>} />
+                    <Route path="/login" element= {<Login/>} />                                      
+                </Routes>
+                
+            </CartProvider>
+        </AuthProvider>
+       
         
         </>
     )
 }
-export default app;
+export default App;
